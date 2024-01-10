197977479d8f3b42f40afb6bcb9fb63045d2cae5
evertonbzr <evertonfrnds@gmail.com>
2024-01-09 23:41:40 -0300

    Update pomme.json and init.ts files

--- packages/cli/src/lib/init.ts
+++ packages/cli/src/lib/init.ts
@@ -3,15 +3,15 @@ import { join } from 'path';
 
 export function init() {
 	const pommeJson = {
+		$schema:
+			'https://raw.githubusercontent.com/evertonbzr/pommejs/master/static/pomme-file-schema.json',
 		input: {
-			targetUrl: '',
 			axios: {
-				baseUrl: '',
+				baseUrl: 'https://jsonplaceholder.typicode.com',
 			},
+			targetUrl: 'http://localhost:3000/pomme',
 		},
-		output: {
-			path: './generated',
-		},
+		output: { path: '.generated' },
 	};
 
 	writeFileSync(
